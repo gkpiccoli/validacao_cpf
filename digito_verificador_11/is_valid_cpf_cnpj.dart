92c110edf3868926c6e1dc6f54d3bc3b3932d282
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-09-05 23:02:04 -0300

    📦 NEW: CNPJ checker

--- digito_verificador_11/is_valid_cpf_cnpj.dart
+++ digito_verificador_11/is_valid_cpf_cnpj.dart
@@ -14,17 +14,70 @@ String applyMaskToCNPJ(String cnpj) {
 
 // Function to validate CPF
 bool isValidCPF(String cpf) {
-  // Your CPF validation logic here
-  // ...
-  return true;  // Placeholder
+  int calcDigit(String slice, List<int> weights) {
+    // Calcular a soma dos dígitos ponderados
+    final sum = slice
+        .split('')
+        .asMap()
+        .map((i, digit) => MapEntry(i, int.parse(digit) * weights[i]))
+        .values
+        .reduce((a, b) => a + b);
+
+    // Calcular o módulo e o dígito verificador
+    final int mod = sum % 11;
+    return (mod < 2) ? 0 : 11 - mod;
+  }
+
+  // Pesos para os primeiros e segundos dígitos de verificação
+  final weights1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+  final weights2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+  // Partes do CPF para calcular os dígitos de verificação
+  final slice1 = cpf.substring(0, 9);
+  final slice2 = cpf.substring(0, 10);
+
+  // Calcular os dígitos de verificação
+  final digit1 = calcDigit(slice1, weights1);
+  final digit2 = calcDigit(slice2, weights2);
+
+  // Validar o CPF
+  return '$digit1$digit2' == cpf.substring(9);
 }
 
 // Function to validate CNPJ
 bool isValidCNPJ(String cnpj) {
-  // Your CNPJ validation logic here
-  // ...
-  return true;  // Placeholder
+  // Function to calculate the verification digit
+  int calcDigit(String slice, List<int> weights) {
+    // Calculate the sum of the weighted digits
+    int sum = 0;
+    for (int i = 0; i < slice.length; i++) {
+      sum += int.parse(slice[i]) * weights[i];
+    }
+
+    // Calculate the remainder of the sum when divided by 11
+    int remainder = sum % 11;
+
+    // If the remainder is less than 2, the verification digit is 0.
+    // Otherwise, it is 11 minus the remainder.
+    return remainder < 2 ? 0 : 11 - remainder;
+  }
+
+  // Weights for the first and second verification digits of CNPJ
+  final List<int> weights1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  final List<int> weights2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+  // Slices of the CNPJ to calculate the verification digits
+  final String slice1 = cnpj.substring(0, 12);
+  final String slice2 = cnpj.substring(0, 13);
+
+  // Calculate the verification digits
+  final int digit1 = calcDigit(slice1, weights1);
+  final int digit2 = calcDigit(slice2, weights2);
+
+  // Validate the CNPJ
+  return '$digit1$digit2' == cnpj.substring(12);
 }
+
 
 // Function to identify and validate CPF or CNPJ
 bool isValidCPForCNPJ(String input) {
